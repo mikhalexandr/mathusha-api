9cedddec41b7f84c50c0ea56079d0d656c3bbac8
Alexandr <mikhalchenkov22@gmail.com>
2024-05-28 09:12:26 +0300

    ♻️refactor: something

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 from data import db_session
+from resources import *
+
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-api = Api(app)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["JSON_AS_ASCII"] = False
+
+
+@app.route("/")
+def check_work():
+    return "OK"
+
+
+def add_resources():
+    api = Api(app)
 
 
 def main():
+    if not os.path.isdir("db"):
+        os.mkdir("db")
     db_session.global_init("db/MathGenerator.db")
-    app.run(port=8080, host="127.0.0.1")
+    add_resources()
+    app.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
