dfab21a1a9f9656d0c355e4964b81a78f6e04a5c
Alexandr <mikhalchenkov22@gmail.com>
2024-06-08 14:38:13 +0300

    ♻️refactor: excel parser

--- misc/excel_theme_parser.py
+++ misc/excel_theme_parser.py
@@ -5,27 +5,24 @@ from data.topics import Topic
 from data.tasks import Task
 
 
-# todo: переделать под один sheet_name
-def excel_to_db(excel_file_path):
+def excel_to_db(excel_file_path, sheet_name):
     session = db_session.create_session()
-    excelFile = pd.ExcelFile(excel_file_path)
-    for sheet_name in excelFile.sheet_names:
-        df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
-        theme = Topic(
-            name=sheet_name,
-            image=str(df.iloc[0, 3]),
-            description=str(df.iloc[1, 3])
+    df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+    theme = Topic(
+        name=sheet_name,
+        photo=str(df.iloc[0, 3]),
+        description=str(df.iloc[1, 3])
+    )
+    session.add(theme)
+    for i in range(len(df)):
+        problem = str(df.iloc[i, 0])
+        solution = str(df.iloc[i, 1])
+        complexity = int(df.iloc[i, 2])
+        task = Task(
+            topic_id=sheet_name,
+            problem=problem,
+            solution=solution,
+            complexity=complexity
         )
-        session.add(theme)
-        for i in range(len(df)):
-            problem = str(df.iloc[i, 0])
-            solution = str(df.iloc[i, 1])
-            complexity = int(df.iloc[i, 2])
-            expression = Task(
-                topic_id=sheet_name,
-                problem=problem,
-                solution=solution,
-                complexity=complexity
-            )
-            session.add(expression)
+        session.add(task)
     session.commit()
