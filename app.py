be71c03e8cc1c1c02a1fd7b10b2f89ee66b4c5db
Alexandr <mikhalchenkov22@gmail.com>
2024-06-10 21:28:18 +0300

    🐛fix: fix pack

--- app.py
+++ app.py
@@ -61,3 +61,7 @@ api.add_resource(AdminStatisticsResource, '/api/admin/statistics')
 def main():
     # create_default_data()
     app.run(host='0.0.0.0')
+
+
+if __name__ == '__main__':
+    main()
