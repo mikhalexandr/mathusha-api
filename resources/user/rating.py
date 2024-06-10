158fbbde75e67d76f65938929567181d163c1259
Alexandr <mikhalchenkov22@gmail.com>
2024-06-10 19:50:39 +0300

    🐛fix: fix pack

--- resources/user/rating.py
+++ resources/user/rating.py
@@ -1,4 +1,4 @@
-from flask import g, request, send_from_directory
+from flask import g, send_from_directory
 from flask_restful import Resource
 
 from keycloak_integration import authenticate
@@ -26,31 +26,9 @@ class RatingResource(Resource):
         rating = sorted(rating, key=lambda x: x['rating'], reverse=True)
         user_index = [x for x in range(len(rating)) if rating[x]["id"] == g.user_id][0]
         user_info = rating[user_index], user_index + 1
-        list_leaders = []
-        if len(rating) == 1:
-            leader = rating[0]
-            list_leaders.append(leader['id'])
-        elif len(rating) == 2:
-            leader1, leader2 = rating[0], rating[1]
-            list_leaders.append(leader1['id'])
-            list_leaders.append(leader2['id'])
-        else:
-            leader1, leader2, leader3 = rating[0], rating[1], rating[2]
-            list_leaders.append(leader1['id'])
-            list_leaders.append(leader2['id'])
-            list_leaders.append(leader3['id'])
-        leaders = list((session.query(User.id, User.name).filter
-                   (User.id.in_([i for i in list_leaders])).all()))
-        leaders_info = []
-        for leader in leaders:
-            leaders_info.append({
-                'id': leader[0],
-                'username': leader[1],
-            })
         return {
             'rating': rating,
             'user_info': user_info,
-            'leaders': leaders_info,
         }, 200
 
 
